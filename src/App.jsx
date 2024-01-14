218a321e9ec0e278bb135a34cfbfd0eaa254e466
Amisha PRIYA <priyaamisha03@gmail.com>
2024-01-15 03:18:43 +0530

    Rendering list data dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,8 @@ function App() {
           <h2>Core concepts</h2>
           <ul>
             {/* Attributes like title, description which are passed to a react component are known as props */}
-            <CoreConcept
+
+            {/* <CoreConcept
               title={CORE_CONCEPTS[0].title}
               description={CORE_CONCEPTS[0].description} 
               image={CORE_CONCEPTS[0].image}
@@ -42,15 +43,21 @@ function App() {
               title={CORE_CONCEPTS[1].title}
               description={CORE_CONCEPTS[1].description} 
               image={CORE_CONCEPTS[1].image}
-            />
+            /> */}
+
             {/* Shorter alternative:
             <CoreConcepts {...CORE_CONCEPTS[0]}/> */}
-            <CoreConcept
+
+            {/* <CoreConcept
               title={CORE_CONCEPTS[2].title}
               description={CORE_CONCEPTS[2].description} 
               image={CORE_CONCEPTS[2].image}
             />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} /> */}
+
+            {/* Dynamic rendering of list of data  */}
+            {/* Every child in the list should have a unique 'key' prop warning comes on console. To remove it, add a key prop and assign title value to it. */}
+            {CORE_CONCEPTS.map((coreConcept) => (<CoreConcept key={coreConcept.title} {...coreConcept} />))}
           </ul>
         </section>
 
