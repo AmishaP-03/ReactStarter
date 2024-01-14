11da964741f77c4179f8e0b4a7a0bc34befc2eaf
Amisha PRIYA <priyaamisha03@gmail.com>
2024-01-15 02:29:01 +0530

    Render dynamic content for each tab selection (components being the default one)

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,14 @@ import TabButton from './components/TabButton.jsx';
 
 // Since it is a named export and not a default export, we will use curly brackets with its name while importing it
 import {
-  CORE_CONCEPTS
+  CORE_CONCEPTS,
+  EXAMPLES
 } from './data.js';
 
 function App() {
   // React hooks must be called only inside the component function or other react hooks, that too at the top level and not inside any nested code.
   // selectedValue is always recreated whenever setSelectedValue is called.
-  const [selectedValue, setSelectedValue] = useState('Please click on a button');
+  const [selectedValue, setSelectedValue] = useState('components');
 
   function handleSelect(selection) {
     // Upon calling setSelectedValue here, React internally schedules the state update (& re-execute the App component function).
@@ -72,8 +73,22 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
+
           {/* Get updated values here, since App component is re-executed after each update */}
-          {selectedValue}
+          {/* {selectedValue} */}
+
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedValue].title}</h3>
+            <p>{EXAMPLES[selectedValue].description}</p>
+
+            {/* The <pre> tag defines preformatted text.
+            Text in a <pre> element is displayed in a fixed-width font, and the text preserves both spaces and line breaks.
+            The text will be displayed exactly as written in the HTML source code. */}
+            <pre>
+              {/* The <code> tag is used to define a piece of computer code. The content inside is displayed in the browser's default monospace font. */}
+              <code>{EXAMPLES[selectedValue].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
