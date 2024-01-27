1ac1970b312cdbce9ba95a1f7f95c84d94d6647f
Amisha PRIYA <priyaamisha03@gmail.com>
2024-01-27 15:28:44 +0530

    Building multiple JSX slots

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -7,6 +7,7 @@ import {
 } from '../data.js';
 import TabButton from './TabButton.jsx';
 import Section from './Section.jsx';
+import Tabs from './Tabs.jsx';
 
 
 export default function Examples() {
@@ -22,61 +23,73 @@ export default function Examples() {
         console.log(selectedValue); // get old values only
     }
 
+    const tabContent = selectedValue 
+    ? (<div id="tab-content">
+        <h3>{EXAMPLES[selectedValue].title}</h3>
+        <p>{EXAMPLES[selectedValue].description}</p>
+
+        {/* The <pre> tag defines preformatted text.
+            Text in a <pre> element is displayed in a fixed-width font, and the text preserves both spaces and line breaks.
+            The text will be displayed exactly as written in the HTML source code. */}
+        <pre>
+        {/* The <code> tag is used to define a piece of computer code. The content inside is displayed in the browser's default monospace font. */}
+        <code>{EXAMPLES[selectedValue].code}</code>
+        </pre>
+    </div>) 
+    : (<p>Please select a topic</p>);
+
     return (
-        <Section title="Examples" id="examples">
-        {/* Helps us create a list of buttons */}
-        <menu>
-          {/* Similar to how we use a regular button */}
+    // APPROACH 1 - All the tabs are handled here
 
-          {/* We cannot pass parameters directly to handleSelect function here since that would mean that we are invoking the function when code executes for the first time itself.
-          Whereas our aim is to trigger it only when a button is clicked. Hence we want React to call the function upon button click.
+    //     <Section title="Examples" id="examples">
+    //     {/* Helps us create a list of buttons */}
+    //     <menu>
+    //       {/* Similar to how we use a regular button */}
 
-          In order to pass parameters to the handleSelect function:
-          1. Set the value of onSelect prop to an arrow function instead (or use anonymous function like: <TabButton onSelect={function(){handleSelect}}>Components</TabButton>)
-          2. Pass params to handleSelect function
+    //       {/* We cannot pass parameters directly to handleSelect function here since that would mean that we are invoking the function when code executes for the first time itself.
+    //       Whereas our aim is to trigger it only when a button is clicked. Hence we want React to call the function upon button click.
 
-          This will make sure that upon button click, React will invoke the set arrow function, which will in turn invoke the handleSelect function with parameters. */}
-          <TabButton isSelected={selectedValue === 'components'} onSelect={() => handleSelect('components')}>Components</TabButton>
-          <TabButton isSelected={selectedValue === 'jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-          <TabButton isSelected={selectedValue === 'props'} onSelect={() => handleSelect('props')}>Props</TabButton>
-          <TabButton isSelected={selectedValue === 'state'} onSelect={() => handleSelect('state')}>State</TabButton>
+    //       In order to pass parameters to the handleSelect function:
+    //       1. Set the value of onSelect prop to an arrow function instead (or use anonymous function like: <TabButton onSelect={function(){handleSelect}}>Components</TabButton>)
+    //       2. Pass params to handleSelect function
 
-          {/* Using props wrapping to even simplify the above procees. Need not create a new attribute onSelect. We can directly defined onClick here and pass it by wrapping to the child component.
-                <TabButton isSelected={selectedValue === 'components'} onClick={() => handleSelect('components')}>Components</TabButton>
+    //       This will make sure that upon button click, React will invoke the set arrow function, which will in turn invoke the handleSelect function with parameters. */}
+    //       <TabButton isSelected={selectedValue === 'components'} onSelect={() => handleSelect('components')}>Components</TabButton>
+    //       <TabButton isSelected={selectedValue === 'jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+    //       <TabButton isSelected={selectedValue === 'props'} onSelect={() => handleSelect('props')}>Props</TabButton>
+    //       <TabButton isSelected={selectedValue === 'state'} onSelect={() => handleSelect('state')}>State</TabButton>
 
-                In TabButton component:
+    //       {/* Using props wrapping to even simplify the above procees. Need not create a new attribute onSelect. We can directly defined onClick here and pass it by wrapping to the child component.
+    //             <TabButton isSelected={selectedValue === 'components'} onClick={() => handleSelect('components')}>Components</TabButton>
 
-                export default function TabButton({isSelected, children, ...props}) {
-                    return (
-                        <li>
-                            <button className={isSelected ? 'active' : undefined} {...props}>{children}</button>
-                        </li>
-                    );
-                }
-          */}
-        </menu>
+    //             In TabButton component:
 
-        {/* Get updated values here, since App component is re-executed after each update */}
-        {/* {selectedValue} */}
+    //             export default function TabButton({isSelected, children, ...props}) {
+    //                 return (
+    //                     <li>
+    //                         <button className={isSelected ? 'active' : undefined} {...props}>{children}</button>
+    //                     </li>
+    //                 );
+    //             }
+    //       */}
+    //     </menu>
 
-        {selectedValue && (
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedValue].title}</h3>
-            <p>{EXAMPLES[selectedValue].description}</p>
+    //     {/* Get updated values here, since App component is re-executed after each update */}
+    //     {/* {selectedValue} */}
+    //     {tabContent}
+    //   </Section>
 
-            {/* The <pre> tag defines preformatted text.
-                    Text in a <pre> element is displayed in a fixed-width font, and the text preserves both spaces and line breaks.
-                    The text will be displayed exactly as written in the HTML source code. */}
-            <pre>
-              {/* The <code> tag is used to define a piece of computer code. The content inside is displayed in the browser's default monospace font. */}
-              <code>{EXAMPLES[selectedValue].code}</code>
-            </pre>
-          </div>
-        )}
-
-        {!selectedValue && (
-          <p>Please select a topic</p>
-        )}
-      </Section>
+    // APPROCH 2 - Using a child component
+    <Section title="Examples" id="examples">
+        {/* // JSX code can be used as values, the only thing is that whenever it is, we should have one root element only (<></>), not multiple sibling elements */}
+        <Tabs buttons={<>
+            <TabButton isSelected={selectedValue === 'components'} onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton isSelected={selectedValue === 'jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton isSelected={selectedValue === 'props'} onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton isSelected={selectedValue === 'state'} onSelect={() => handleSelect('state')}>State</TabButton>
+        </>}>
+            {tabContent}
+        </Tabs>
+    </Section>
     );
 }
